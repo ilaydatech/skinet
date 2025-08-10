fcc558d99961bb819b4e150e7d4556c5672eb50e
ilaydasumer1 <ilayda_sumer21@outlook.com>
2025-08-10 16:47:46 +0300

    16. Kısım Tamamlandı: Stripe ile ödeme entegrasyonu ve Adres,Kargo, Ödeme adımları tamamlandı.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38677d09ebbd42789b3eb68909ac7b14de4f4b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4565473240620c86924ee7d501ad7390c17f105e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38677d09ebbd42789b3eb68909ac7b14de4f4b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4565473240620c86924ee7d501ad7390c17f105e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
